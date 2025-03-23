c20ce151f2e0007bbc0ccc4068f4db07bbe73114
JapySirait <jappysirait1808@gmail.com>
2025-03-23 22:12:40 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,12 @@ import 'package:carilaundry2/pages/profil.dart';
 import 'package:carilaundry2/pages/single_order.dart';
 import 'package:carilaundry2/pages/main_container.dart';
 import 'package:carilaundry2/utils/constants.dart';
+import 'package:carilaundry2/pages/order_history.dart';
+import 'package:carilaundry2/pages/notifikasi.dart';
 
 // Global key for app-wide SnackBars
 final GlobalKey<ScaffoldMessengerState> rootScaffoldMessengerKey =
     GlobalKey<ScaffoldMessengerState>();
-
-// import 'package:carilaundry2/widgets/top_bar.dart';
 
 void main() {
   runApp(MyApp());
@@ -95,12 +95,16 @@ Route<dynamic> _onGenerateRoute(RouteSettings settings) {
           return MainContainer(initialIndex: 3);
         },
       );
-    // case "/profile":
-    //   return MaterialPageRoute(
-    //     builder: (BuildContext context) {
-    //       return ProfilePage();
-    //     },
-    //   );
+    case "/order-history":
+      return MaterialPageRoute(builder: (context) => OrderHistoryPage());
+    case "/notifikasi":
+      final args = settings.arguments as Map<String, dynamic>? ?? {};
+      print("Data yang dikirim ke NotifikasiPage: $args"); 
+      return MaterialPageRoute(
+        builder: (context) => NotifikasiPage(
+          notifications: args['notifications'] as List<String>? ?? [],
+        ),
+      );
     default:
       return MaterialPageRoute(
         builder: (BuildContext context) {
